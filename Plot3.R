d10bf5f0a9ebdf3213f76f673fd06496f5e85b5e
Iyke Fred <48203770+Fredase@users.noreply.github.com>
2019-08-06 16:32:39 +0100

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -15,11 +15,12 @@ sub2 <- as.numeric(power$Sub_metering_2)
 sub3 <- as.numeric(power$Sub_metering_3)
 
 #Create a png file to make the plot
-dev.copy(png, file = "Plot2.png", width = 480, height = 480)
+dev.copy(png, file = "Plot3.png", width = 480, height = 480)
 
 # Create the plot
 plot(dateTime, sub1, type = "l", xlab = " ", ylab = "Energy sub metering")
 lines(dateTime, sub2, type = "l", col = "red")
 lines(dateTime, sub3, type = "l", col = "blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 2.5, col = c("black", "red", "blue"))
- 
+
+dev.off()
